ee40bff09be56b92cb953c25fb730805e676a227
U6hufaROcPcrwEpk6Mbg <rwilco@live.ca>
2016-04-14 22:01:37 -0400

    ajout commentaire transactions

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+/**
+ * \file Main.java
+ * \brief Fichier principal pour lancer les tests
+ * \author Étienne Chalifour 111011736 B-IFT, Marc-Gabriel Morin-Grandmont 111103591 B-IFT, Simon Rodier 111083079 B-IFT
+ * \date Avril 2016
+ *
+ * Travail pratique #3
+ *
+ */
 import java.lang.System;
 
 public class Main {
